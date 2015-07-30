5253dcdf06580d6230f854c3ac5d354e7823f022
Michelle Krejci <krejci@palantir.net>
2015-07-29 22:07:18 -0400

    Our first Drupal executable.

---
+++ build/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+path=$(dirname "$0")
+base=$(cd $path/.. && pwd)
+drush="drush $drush_flags -y -r $base/www"
+
+chmod -R +w $base/www/sites/default
+chmod -R +w $base/cnf
+
+echo "Symlink settings.php into our Drupal."
+ln -sf $base/cnf/settings.php $base/www/sites/default/
+echo "Installing Drupal like a boss."
+$drush si --site-name=no-excuses --account-pass=admin
