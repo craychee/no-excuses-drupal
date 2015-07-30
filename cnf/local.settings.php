9a0e7a9f3b42789da12ecb4fe33180bd92f37f95
Michelle Krejci <krejci@palantir.net>
2015-07-29 22:07:03 -0400

    Add a local.settings.php.

---
+++ cnf/local.settings.php
@@ -0,0 +1,12 @@
+<?php
+$db_url='mysql://default:default@localhost/default';
+$databases=array('default' => array(
+    'default' => array(
+        'database' => 'default',
+        'username' => 'default',
+        'password' => 'default',
+        'driver' => 'mysql',
+        'host' => 'localhost',
+    ),
+),);
+$drupal_hash_salt='161302b5bf927369e7c370212318c8f1837b03bbecb94eadb9eeed17a7875d1e';
